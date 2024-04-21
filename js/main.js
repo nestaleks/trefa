ad2161b000dc0344fb7b20b9e58f4b100799fb50
nestaleks <nestaleks83@gmail.com>
2024-04-21 20:48:35 +0300

    1

--- js/main.js
+++ js/main.js
@@ -48,3 +48,30 @@ icons.forEach (icon => {
 
 
 
+$(document).ready(function() {
+	$('.popup__link').magnificPopup({
+		disableOn: 900,
+		type: 'iframe',
+		mainClass: 'mfp-fade',
+		removalDelay: 160,
+		preloader: false,
+		fixedContentPos: false
+	});
+	$('.grid-wrapper').magnificPopup({
+		delegate: 'a',
+		type: 'image',
+		tLoading: 'Loading image #%curr%...',
+		mainClass: 'mfp-img-mobile',
+		gallery: {
+			enabled: true,
+			navigateByImgClick: true,
+			preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+		},
+		image: {
+			tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
+			titleSrc: function(item) {
+				return item.el.attr('title') + '<small></small>';
+			}
+		}
+	});
+});
